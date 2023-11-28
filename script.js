942eaf6e54407d1895d90763ee915834b57d0dd1
Paul Lebée <paul.lebee@epita.fr>
2023-11-28 21:32:56 +0100

    NEW Better articles

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    $('.owl-carousel').owlCarousel({
+    $('.owl-main').owlCarousel({
         loop: true, 
         margin: 20,
         nav: true,
@@ -16,6 +16,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
             }
         }
     });
+
+    $('.owl-modal').owlCarousel({
+        items: 1,
+        margin: 10,
+        loop: true, 
+        nav: true,
+        dots: true,
+        // mouseDrag : true,
+    });
+    
     // Associez les boutons de contrôle aux flèches
     $('.custom-nav button.next').on('click', function() {
     $('.owl-carousel').trigger('next.owl.carousel');
@@ -39,8 +49,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 
     profil.addEventListener('mouseover', function() {
-        clearInterval(clickMe);
         profil.style.filter = "grayscale(0)";
+        clearInterval(clickMe);
+       
     });
 
     profil.addEventListener('mouseout', function() {
@@ -54,34 +65,43 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 
     let colorDegree = 0;
-    document.addEventListener('scroll', function() {
-        colorDegree += 5;
-        if(colorDegree >= 360) {
-            colorDegree = 0;
-        }
-        background.style.filter = "hue-rotate("+colorDegree+"deg)";
-    });
+    // document.addEventListener('scroll', function() {
+    //     colorDegree += 5;
+    //     if(colorDegree >= 360) {
+    //         colorDegree = 0;
+    //     }
+    //     background.style.filter = "hue-rotate("+colorDegree+"deg)";
+    // });
 
     let profilDiv = document.getElementById('profilDiv');
 
     document.addEventListener('mousemove', (e) => {
-        console.log(window.innerWidth/2);
         const x = e.clientX-(window.innerWidth/2);
         const y = e.clientY;
 
         const imageContainer = document.querySelector('.image-container');
 
         imageContainer.style.clipPath = `circle(50px at ${x}px ${y}px)`;
-        console.log(imageContainer.style.clipPath);
     });
 
-    let clickMe =
-        setInterval(function() {
-            profil.style.filter = "grayscale(1) drop-shadow(0 0 2rem rgb(160, 160, 160))";
-            setTimeout(function() {
-                profil.style.filter = "grayscale(1) drop-shadow(0 0 0.75rem rgb(160, 160, 160))";
-            }, 500);
-        }, 1000);
+    
+    let clickMe = setInterval(function() {
+        profil.style.filter = "grayscale(1) drop-shadow(0 0 2rem rgb(160, 160, 160))";
+        setTimeout(function() {
+            profil.style.filter = "grayscale(1) drop-shadow(0 0 0.75rem rgb(160, 160, 160))";
+        }, 500);
+    }, 1000);
+    
+    var owl = $('.owl-carousel');
+    owl.owlCarousel();
 
+    // Listen to owl events:
+    owl.on('changed.owl.carousel', function(event) {
+        colorDegree += 50;
+        if(colorDegree >= 360) {
+            colorDegree = 0;
+        }
+        background.style.filter = "hue-rotate("+colorDegree+"deg)";
+    })
 
 });
